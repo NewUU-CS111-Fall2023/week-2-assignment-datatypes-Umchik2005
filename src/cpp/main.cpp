f52a762bd9f81876e387e54d1f86af620832c551
Muhammad Uamr Toshpo'latov <m.toshpolatov@newuu.uz>
2023-10-24 10:27:59 +0500

    test

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -4,22 +4,28 @@
  */
 
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 #include "task_1.h"
+#include "task_2.h"
 
 int main() {
-    std::cout << "Task 1" << std::endl;
-    int year;
-    std::cout << "Enter a year: ";
-    std::cin >> year;
-    bool leapYear = isLeapYear(year);
-    if(leapYear == true){
-        std::cout << year << " is leap year";
-    } else {
-        std::cout << year << " isn't leap year";
-    }
-    std::cout << "\n";
-    // call for task 1
+//     std::cout << "Task 1" << std::endl;
+//     int year;
+//     std::cout << "Enter a year: ";
+//     std::cin >> year;
+//     bool leapYear = isLeapYear(year);
+//     if(leapYear == true){
+//         std::cout << year << " is leap year";
+//     } else {
+//         std::cout << year << " isn't leap year";
+//     }
+//     std::cout << "\n";
+//     // call for task 1
+    
     std::cout << "Task 2" << std::endl;
+    secondTask();
+    std::cout << "\n";
     // call for task 2
     std::cout << "Task 3" << std::endl;
     // call for task 3
